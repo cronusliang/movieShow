31f28e0b4b8412dcdd6fc8b48b2c98b4053e2d2a
cronus <cronusliang@gmail.com>
2017-09-03 22:01:41 +0800

    modify Annotations

--- get_movie_data.py
+++ get_movie_data.py
@@ -10,7 +10,12 @@ from media import Subject, Movie
 def get_theaters():
     subjects = []
     theaters_url = 'https://api.douban.com/v2/movie/in_theaters'
-    param  = {'apikey':'0b2bdeda43b5688921839c8ecb20399b', 'city':'%E5%8C%97%E4%BA%AC','start':'0','count':'15','client':'','udid':''}
+    param  = {'apikey':'0b2bdeda43b5688921839c8ecb20399b',
+              'city':'%E5%8C%97%E4%BA%AC',
+              'start':'0',
+              'count':'15',
+              'client':'',
+              'udid':''}
 
     r = requests.get(theaters_url, params = param)
     if r.status_code == requests.codes.ok:
@@ -26,12 +31,16 @@ def get_theaters():
 # 获取具体电影的资源
 def get_movie(id):
     subject_url = 'http://api.douban.com/v2/movie/subject/'+id
-    param = {'apikey':'0b2bdeda43b5688921839c8ecb20399b', 'city':'%E5%8C%97%E4%BA%AC','client':'','udid':''}
+    param = {'apikey':'0b2bdeda43b5688921839c8ecb20399b',
+             'city':'%E5%8C%97%E4%BA%AC',
+             'client':'',
+             'udid':''}
     r = requests.get(subject_url,params=param)
     if r.status_code == requests.codes.ok:
        data = json.loads(r.content)
        return Movie(data['title'], data['original_title'], data['summary'], data['images']['large'], data['trailer_urls'][0])
 
+# 获取最近上映的电影资源 return list
 def get_movies():
   movies = []
   subjects = get_theaters()
--- media.py
+++ media.py
@@ -1,8 +1,9 @@
 import webbrowser
 
-
 class Movie():
+    '''Movie presentation entity class '''
     def __init__(self,title,original_title,movie_storyline,poster_image,trailer_youtube):
+        """init movie Attributes"""
         self.title = title
         self.original_title = original_title
         self.storyline = movie_storyline
@@ -10,11 +11,14 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """open video by browser"""
         webbrowser.open(self.trailer_youtube_url)
 
 
 class Subject():
+    '''Recently released film parse entity class'''
     def __init__(self,id,title,alt,image):
+        """init Subject Attributes"""
         self.id = id
         self.title = title
         self.alt = alt
